7cfe6a4ce747288289c1eca468a0fd81112ddd23
Yukina Tatsuta <ivy1200t@gmail.com>
2024-08-25 03:41:07 +0900

    make home screen

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:jewelry_custom_flutter/widgets/foot_buttons.dart'; // 実際のパスに合わせてください
+
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -8,39 +10,57 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Flutter Demo Home Page'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      // appBar: AppBar(
+      //   backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+      //   title: const Text('Home'),
+      // ),
+      body: Stack(
+        children: <Widget>[
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Image.asset(
+                  'assets/images/test_jewel.png',
+                  cacheWidth: 200,
+                  cacheHeight: 200,
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+          ),
+          Align(
+            alignment: Alignment.bottomCenter,
+            child: Padding(
+              padding: const EdgeInsets.all(100.0),
+              child: ElevatedButton(
+                onPressed: () {
+                },
+                child: const Text(
+                  'みがく',
+                  style:TextStyle(fontSize: 24)
+                ),
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFFE5F9FF), // ボタンの背景色
+                  foregroundColor: const Color(0xFF006686), // ボタン上のテキストの色
+                  side: BorderSide(color: Color(0xFF95DFF6), width: 8),//枠の設定
+                  // shape: const StadiumBorder(),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                  fixedSize: Size(300, 100),
+                ),
+              ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+      bottomNavigationBar: Container(
+        color: Colors.grey[300], // 背景の灰色
+        height: 150, // 背景の高さ
+        child: FooterButtons(), // フッターボタンを追加
       ),
     );
   }
---
+++ lib/widgets/foot_buttons.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+class FooterButtons extends StatelessWidget {
+  const FooterButtons({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomAppBar(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: <Widget>[
+            Expanded(
+              child: _buildButton(context, 'ホーム', Icons.home),
+            ),
+            SizedBox(width: 8), // ボタンの間に縦のスペースを追加
+            Expanded(
+              child: _buildButton(context, 'プロフィール', Icons.person),
+            ),
+            SizedBox(width: 8), // ボタンの間に縦のスペースを追加
+            Expanded(
+              child: _buildButton(context, 'フレンド', Icons.group),
+            ),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildButton(BuildContext context, String label, IconData icon) {
+    return ElevatedButton(
+      style: ElevatedButton.styleFrom(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(16.0), // ボタンの角を丸くする
+        ),
+        backgroundColor: Colors.amber[200], // ボタンの背景色
+        foregroundColor: Colors.yellow[900], // ボタン上のテキストの色
+        // minimumSize: Size(60, 60), // ボタンの最小サイズ
+        padding: EdgeInsets.symmetric(vertical: 10.0), // ボタンの内側の余白（縦）
+      ),
+      // tooltip: label,
+      onPressed: () {
+        // 各ボタンが押されたときの処理をここに記述
+        // 例: Navigator.pushNamed(context, '/${label}');
+      },
+      
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Icon(icon, size: 30), // アイコンのサイズ
+          Text(label,style:TextStyle(fontSize: 12)),
+        ],
+      ),
+
+    );
+  }
+}
